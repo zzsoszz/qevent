9f73e85b423063361ad2639eee3d874ddf2914b2
hyq2015 <18030638805@163.com>
2017-04-17 17:27:52 +0800

    update

--- qevent.js
+++ qevent.js
@@ -1,8 +1,6 @@
 /*
 	https://learn.jquery.com/events/event-extensions/
 	http://benalman.com/news/2010/03/jquery-special-events/#api-setup
-	https://github.com/benmajor/jQuery-Touch-Events#7-utility-functions
-	https://github.com/benmajor/jQuery-Touch-Events/blob/master/src/jquery.mobile-events.js
 */
  (function($){
 	function GetSlideAngle(dx, dy) {
@@ -54,15 +52,14 @@
                     y: 0
 			};
 			$(this).on(settings.startevent, function (e) {
-				e.preventDefault();
-			    originalCoord.x = (e.originalEvent.targetTouches) ? e.originalEvent.targetTouches[0].pageX : e.pageX;
-				originalCoord.y = (e.originalEvent.targetTouches) ? e.originalEvent.targetTouches[0].pageY : e.pageY;
+				//e.preventDefault();
+			    originalCoord.x = (touchCapable) ? e.originalEvent.targetTouches[0].pageX : e.pageX;
+				originalCoord.y = (touchCapable) ? e.originalEvent.targetTouches[0].pageY : e.pageY;
 			});
 			$(this).on(settings.endevent, function (e) {
-				e.preventDefault();
 				$this = $(e.currentTarget);
-			    finalCoord.x = (e.originalEvent.targetTouches) ? e.originalEvent.targetTouches[0].pageX : e.pageX;
-				finalCoord.y = (e.originalEvent.targetTouches) ? e.originalEvent.targetTouches[0].pageY : e.pageY;
+			    finalCoord.x = (touchCapable) ? e.originalEvent.changedTouches[0].pageX : e.pageX;
+				finalCoord.y = (touchCapable) ? e.originalEvent.changedTouches[0].pageY : e.pageY;
 			    var direction = GetSlideDirection(originalCoord.x, originalCoord.y, finalCoord.x, finalCoord.y);
 			    switch (direction) {
 				        case 0:
@@ -77,7 +74,7 @@
 				            $this.trigger("swipedown");
 				            break;
 				        case 3:
-				        	//alert("向左");
+				        	//向左
 				        	$this.trigger("swipeleft");
 				            break;
 				        case 4:
@@ -98,7 +95,7 @@
 		}
 	};
 
-	$.each({
+	 $.each({
 	        scrollend: 'scrollstart',
 	        swipeup: 'swipe',
 	        swiperight: 'swipe',
@@ -115,3 +112,70 @@
 	});
 	 
 })(jQuery);
+
+
+
+
+
+
+(function($){
+
+  // A collection of elements to which the tripleclick event is bound.
+  var elems = $([]),
+
+    // Initialize the clicks counter and last-clicked timestamp.
+    clicks = 0,
+    last = 0;
+
+  // Click speed threshold, defaults to 500.
+  $.tripleclickThreshold = 500;
+
+  // Special event definition.
+  $.event.special.tripleclick = {
+    setup: function(){
+      // Add this element to the internal collection.
+      elems = elems.add( this );
+
+      // If this is the first element to which the event has been bound,
+      // bind a handler to document to catch all 'click' events.
+      if ( elems.length === 1 ) {
+        $(document).bind( 'click', click_handler );
+      }
+    },
+    teardown: function(){
+      // Remove this element from the internal collection.
+      elems = elems.not( this );
+
+      // If this is the last element removed, remove the document 'click'
+      // event handler that "powers" this special event.
+      if ( elems.length === 0 ) {
+        $(document).unbind( 'click', click_handler );
+      }
+    }
+  };
+
+  // This function is executed every time an element is clicked.
+  function click_handler( event ) {
+    var elem = $(event.target);
+
+    // If more than `threshold` time has passed since the last click, reset
+    // the clicks counter.
+    if ( event.timeStamp - last > $.tripleclickThreshold ) {
+      clicks = 0;
+    }
+
+    // Update the last-clicked timestamp.
+    last = event.timeStamp;
+
+    // Increment the clicks counter. If the counter has reached 3, trigger
+    // the "tripleclick" event and reset the clicks counter to 0. Trigger
+    // bound handlers using triggerHandler so the event doesn't propagate.
+    if ( ++clicks === 3 ) {
+      elem.trigger( 'tripleclick' );
+      clicks = 0;
+    }
+  };
+
+})(jQuery);
+
+
